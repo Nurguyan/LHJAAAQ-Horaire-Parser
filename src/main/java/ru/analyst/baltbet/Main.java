d96ae316c3cc293409acfa098484398466c4bf8c
Nurguyan <centauri368@gmail.com>
2020-11-02 08:48:31 +0300

    reads scripts

--- src/main/java/ru/analyst/baltbet/Main.java
+++ src/main/java/ru/analyst/baltbet/Main.java
@@ -1,35 +1,44 @@
 package ru.analyst.baltbet;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.lhjaaaq.com/fr/stats/horaire.html?season=2359&subSeason=2371&category=1093").get();
-        String title = doc.title();
-//        Elements urls = doc.getElementsByAttributeValueContaining("class", "ellipsis_wrapper link_container");
-//        Elements urls = doc.getElementsByClass( "ellipsis_wrapper link_container");
-        Elements scripts = doc.select("script.schedule_tpl");
-        String relHref = scripts.attr("href");
-        String absHref = scripts.attr("abs:href");
-        for (Element script : scripts) {
-            String type = script.attr("type");
-            if (type.contentEquals("text/x-handlebars-template")) {
-                String scriptData = script.data(); // your text from the script
-                System.out.println(scriptData);
-                break;
+        String url = "https://www.lhjaaaq.com/fr/stats/horaire.html?season=2359&subSeason=2371&category=1093";
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\nkornilov\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        String item = "Sommaire";
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            driver.get(url);
+            final WebElement schedule_container = driver.findElement(By.className("schedule_container"));
+            //String html = schedule_container.getAttribute("innerHTML");
+            List<WebElement> linkList = schedule_container.findElements(By.tagName("a"));
+            for (WebElement link : linkList) {
+                if (link.getText().equals(item)){
+                    System.out.println(link.getText() + " - " + link.getAttribute("href"));
+                    driver.get(link.getAttribute("href"));
+                    final String match_date = driver.findElement(By.className("date_container bg_primary")).getText();
+                    WebElement season_stats = driver.findElement(By.id("season_stats"));
+                    System.out.println(match_date  + ": " + season_stats.getText());
+                }
             }
+        } finally {
+            driver.quit();
         }
-
-//        System.out.println(scripts.size());
-//        System.out.println(title);
-//        System.out.println(scripts.first().getElementsByClass("table_container"));
-//        System.out.println(relHref);
-//        System.out.println(absHref);
     }
 }
