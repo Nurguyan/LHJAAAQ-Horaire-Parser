9ae1383f55ec24fdf72c5819b606ad77125d703e
Nurguyan <centauri368@gmail.com>
2020-11-03 07:45:25 +0300

    added match info

--- src/main/java/ru/analyst/baltbet/Main.java
+++ src/main/java/ru/analyst/baltbet/Main.java
@@ -1,11 +1,12 @@
 package ru.analyst.baltbet;
 
-import jdk.internal.net.http.common.Pair;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,20 +20,65 @@ public class Main {
         return matchesUrls;
     }
 
-    private static Pair<String, String> getMatchInfo(String url, WebDriver driver){
+    private static List<String> getMatchInfo(String url, WebDriver driver){
         driver.get(url);
+        String date = driver.findElement(By.xpath("//div[contains(@class,'date_container bg_primary')]")).getText();
         List<WebElement> teams = driver.findElements(By.xpath("//div[contains(@class,'section season_stats_section')]/div[contains(@class,'table_container')]/table[@class='statistic_displayer']/tbody/tr"));
-        Pair<String, String> info = new Pair<>(teams.get(0).getText(), teams.get(1).getText());
-        return info;
+        List<String> matchInfo = new ArrayList<>(2);
+        teams.forEach((team -> matchInfo.add(date.substring(date.indexOf(' ')+1).replace(",","") + ' ' + team.getText())));
+        return matchInfo;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String statsUrl = "https://www.lhjaaaq.com/fr/stats/horaire.html?season=2359&subSeason=2371&category=1093";
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-//        List<String> matchesUrls = getMatchesUrls(statsUrl, driver);
-        getMatchInfo("https://www.lhjaaaq.com/fr/stats/sommaire.html?season=2359&subSeason=2371&category=1093&game=791202", driver);
+        List<String> matchesUrls = getMatchesUrls(statsUrl, driver);
+
+        FileWriter writer = new FileWriter("stats.csv");
+        char delimiter = ';';
+        writer.append("Day");
+        writer.append(delimiter);
+        writer.append("Month");
+        writer.append(delimiter);
+        writer.append("Year");
+        writer.append(delimiter);
+        writer.append("Time");
+        writer.append(delimiter);
+        writer.append("Team");
+        writer.append(delimiter);
+        writer.append("BP");
+        writer.append(delimiter);
+        writer.append("BC");
+        writer.append(delimiter);
+        writer.append("BP%");
+        writer.append(delimiter);
+        writer.append("BC%");
+        writer.append(delimiter);
+        writer.append("Tirs%");
+        writer.append(delimiter);
+        writer.append("TR%");
+        writer.append(delimiter);
+        writer.append("AN%");
+        writer.append(delimiter);
+        writer.append("DN%");
+        writer.append(delimiter);
+        writer.append("PUN");
+        writer.append(System.getProperty( "line.separator"));
+
+        for (String matchUrl : matchesUrls) {
+            List<String> matchInfos = getMatchInfo(matchUrl, driver);
+            for (String matchInfo : matchInfos) {
+                String line = matchInfo.replace(" %", "") .replace(' ', delimiter);
+                System.out.println(line);
+                writer.append(line);
+                writer.append(System.getProperty( "line.separator"));
+            }
+        }
+
+        writer.flush();
+        writer.close();
         driver.quit();
     }
 }
